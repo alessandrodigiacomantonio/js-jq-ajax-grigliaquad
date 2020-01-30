50f013188903afb475ff4b87246d0cbe62e17cee
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-30 16:31:42 +0100

    Generazione griglia

--- js/script.js
+++ js/script.js
@@ -1,22 +1,19 @@
 $(document).ready(function() {
+
   var row = document.getElementById("row").innerHTML;
   var  genRow = Handlebars.compile(row);
   var column = document.getElementById("column").innerHTML;
   var  genColumn = Handlebars.compile(column);
-  for (var i = 0; i < 6; i++) {
 
+  for (var i = 0; i < 6; i++) {
     var rowNumber = { row_number: i+1,};
-    console.log(rowNumber.row_number);
-    console.log($('.row').attr('data-row', i+1 ));
     var newRow = genRow(rowNumber);
     $('#main').append(newRow);
-
-    for (var j = 0; j < 6; j++) {
-
-      var columnNumber = { column_number: (j+1),};
-      var newColumn = genColumn(columnNumber);
-      $('.row').attr('data-row', i+1 ).append(newColumn);
-    }
+  }
+  for (var j = 0; j < 6; j++) {
+    var columnNumber = { column_number: (j+1),};
+    var newColumn = genColumn(columnNumber);
+    $('.row').attr('data-row', i+1 ).append(newColumn);
   }
 
 });
